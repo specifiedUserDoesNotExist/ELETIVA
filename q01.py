762f9082e00883c675a4d6d6127d193c81b95b6d
__user__not__found__ <sammael.apoliom@hotmail.com>
2022-12-01 09:18:51 -0300

    Create q01.py

---
+++ q01.py
@@ -0,0 +1,9 @@
+"""
+Dado o tamanho da base e da altura de um retângulo, calcular a sua área e o seu perímetro.
+"""
+b = float(input("Digite base: "))
+h = float(input("Digite altura: "))
+
+area = b*h
+
+print("Área = %.2f" %area)
