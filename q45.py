96d6ff04684a9d3b66dc8d156b4ad18ed5b746fc
__user__not__found__ <sammael.apoliom@hotmail.com>
2022-12-01 10:41:30 -0300

    Create q45.py

---
+++ q45.py
@@ -0,0 +1,13 @@
+"""
+Escreva um programa que leia 5 números, e imprima a média entre eles.
+"""
+
+n1 = float(input("Digite nota 1: "))
+n2 = float(input("Digite nota 2: "))
+n3 = float(input("Digite nota 3: "))
+n4 = float(input("Digite nota 4: "))
+n5 = float(input("Digite nota 5: "))
+
+media = (n1+n2+n3+n4+n5)/5
+
+print("Média = %.2f" %media)
