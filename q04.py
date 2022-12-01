f17108627c46c2ee90ca65df4f8d1572092841d4
__user__not__found__ <sammael.apoliom@hotmail.com>
2022-12-01 09:23:54 -0300

    Create q04.py

---
+++ q04.py
@@ -0,0 +1,11 @@
+"""
+Dado os três lados de um triângulo determinar o perímetro do mesmo.
+"""
+
+l1 = float(input("Digite lado 1: "))
+l2 = float(input("Digite lado 2: "))
+l3 = float(input("Digite lado 3: "))
+
+perimetro = l1+l2+l3
+
+print("Perímetro = %.2f" %perimetro)
