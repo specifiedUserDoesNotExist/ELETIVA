bcedcb20e12d56fcaa802b886211a78a4554634d
__user__not__found__ <sammael.apoliom@hotmail.com>
2022-12-01 09:23:11 -0300

    Create q03.py

---
+++ q03.py
@@ -0,0 +1,11 @@
+"""
+Dado o tamanho do raio de uma circunferência, calcular a área e o perímetro da mesma.
+"""
+
+raio = float(input("Digite raio: "))
+
+area = 3.14*raio*raio
+perimetro = 2*3.14*raio
+
+print("Área = %.2f" %area)
+print("Perímetro = %.2f" %perimetro)
