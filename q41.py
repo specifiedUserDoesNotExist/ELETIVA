4ad75acf033a7cbe3e4ccbcd6966c9c731a7bba8
__user__not__found__ <sammael.apoliom@hotmail.com>
2022-12-05 22:45:12 -0300

    Create q41.py

---
+++ q41.py
@@ -0,0 +1,42 @@
+"""
+Faça um programa que leia três valores (A, B, C) e mostre-os na ordem lida. Em seguida, mostre-os em ordem crescente e decrescente.
+"""
+
+
+a = input("digite o primeiro numero");
+b = input("digite o segundo numero");
+c = input("digite o terceiro numero");
+
+maior = 0;
+meio = 0;
+menor = 0;
+
+
+if a > b and a > c:
+    maior = a
+elif b > a and b > c:
+    maior = b
+elif c > a and c > b:
+    maior = c
+    
+
+if a < b and a < c:
+    menor = a
+elif b < a and b < c:
+    menor = b
+elif c < a and c < b:
+    menor = c
+    
+if a != menor and b != maior:
+    meio = c
+elif a != menor and c != maior:
+    meio = b
+else:
+    meio = a
+    
+    
+print(a,b,c)
+    
+print(maior, meio, menor)
+
+print(menor, meio, maior)
